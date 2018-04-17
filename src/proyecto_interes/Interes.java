58702012d240abde07b3d6012832ba85b84e2b7c
Irene <Irene@DESKTOP-5G5Q3P1>
2018-04-17 07:49:35 +0200

    calcular interes

--- src/proyecto_interes/Interes.java
+++ src/proyecto_interes/Interes.java
@@ -1,20 +1,38 @@
-/*
- * Cálculo de interés simple = capital * redito * tiempo/100.
+package proyecto_interes;
+
+/**
+ *Cálculo de interés simple = capital * redito * tiempo/100.
  * El capital y el tiempo son >0.
  * El redito puede ser negativo o cero.
- * autor: profesor
+ * @author Irene Rey
  */
-package proyecto_interes;
 public class Interes {
+
+    /**
+     * Declarar las variables
+     */
     public int x=2;
     private float capital;
     private float redito;
     private int tiempo;
+
+    /**
+     *
+     * @param capital
+     * @param redito
+     * @param tiempo
+     */
     public Interes(float capital, float redito, int tiempo) {        
         this.capital=capital;
         this.redito=redito;
         this.tiempo=tiempo;
     }
+
+    /**
+     *
+     * @return
+     * @throws Exception
+     */
     public double CalcularInteres()throws Exception{
         if (capital<=0)
             throw (new Exception ("Error. El capital tiene que ser >=0"));
